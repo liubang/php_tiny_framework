d74a38431a6c8a9155004fee9b4c3794523dc0f7
iliubang <it.liubang@qq.com>
2016-03-24 19:53:46 +0800

    add some files

---
+++ Linger/Driver/Cache/Redis.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ |------------------------------------------------------------------
+ | linger.iliubang.cn
+ |------------------------------------------------------------------
+ | @author    : liubang 
+ | @date      : 16/3/24 下午7:51
+ | @copyright : (c) iliubang.cn
+ | @license   : MIT (http://opensource.org/licenses/MIT)
+ |------------------------------------------------------------------
+ */
+
+namespace Linger\Driver\Cache;
+
+class Redis
+{
+
+}
