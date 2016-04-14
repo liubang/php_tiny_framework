9294a4d5bb7cd55ac4aa7052b8fb59d6dcf331de
liubang <liubang@kongfz.com>
2016-04-14 16:50:47 +0800

    尝试做进一步优化

---
+++ Linger/Common/helpers.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ |------------------------------------------------------------------
+ | linger.iliubang.cn
+ |------------------------------------------------------------------
+ | @author    : liubang
+ | @date      : 16/3/23 下午8:28
+ | @copyright : (c) iliubang.cn
+ | @license   : MIT (http://opensource.org/licenses/MIT)
+ |------------------------------------------------------------------
+ */
+
+if (! function_exists('app')) {
+    function app()
+    {
+
+    }
+}
+
+if (! function_exists('_404')) {
+    function _404($code = false)
+    {
+        
+    }
+}
