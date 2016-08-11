4a6f534ca4d77eda048d4084566d6e0fe08e2bee
iliubang <it.liubang@qq.com>
2016-08-11 15:08:19 +0800

    t

--- app/module/home/controller/IndexController.php
+++ app/module/home/controller/IndexController.php
@@ -11,8 +11,8 @@
  */
 
 namespace home\controller;
+
 use Linger\Kernel\Controller;
-use model\UserModel;
 
 class IndexController extends Controller
 {
@@ -28,12 +28,6 @@ class IndexController extends Controller
             ['userId' => 201502, 'userName' => '张海']
         ];
 
-        $userModel = new UserModel();
-
-        var_dump($userModel->addUser(array('name' => 'liubang', 'age' => '23')));
-        \p($userModel->getUserInfo());
-        die;
-
 //        trigger_error("这是一个错误！", E_USER_ERROR);
 //        throw new \Exception('这是一个测试异常，就是要抛出去一个异常');die;
 
@@ -47,10 +41,10 @@ class IndexController extends Controller
         $this->assign('arr', $arr);
         $this->assign('flag', true);
         $this->assign('time', time());
-        //$this->getResponse()->code('500');
-        //$this->getResponse()->header('liubang', 'hh');
-        //$this->getResponse()->json($arr);
-        //$this->getResponse()->json($arr, 'callback');
+        $this->getResponse()->code('500');
+        $this->getResponse()->header('liubang', 'hh');
+        $this->getResponse()->json($arr);
+//        $this->getResponse()->json($arr, 'callback');
         $this->display();
     }
 }
